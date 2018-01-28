31c5e67a94e77904ba2599b1b91885b7061437ea
mgb <mgbmkw@163.com>
2018-01-28 21:57:21 +0800

    backend完整的判断

--- backend/js/controllers/api/Mongoose/register.js
+++ backend/js/controllers/api/Mongoose/register.js
@@ -1,5 +1,6 @@
 const express = require("express")
-const User = require("./link/user")
+const User = require("./link/user_db")
+const help = require("../../../help")
 
 
 let router = express.Router()
@@ -12,23 +13,60 @@ router.post('/', async (req, res, next) => {
   let phone = req.body.params.phone
   let code = req.body.params.code
   let phoneCode = req.body.params.phoneCode
-  console.log(req.body.params.username)
-  console.log(req.body.params)
+
+  let returnStr = {
+    data: {}
+  }
+
+  // 判断非空
+  if (!username)
+    return res.json(help.validator(username, returnStr, 'username'))
+
+  if (!password)
+    return res.json(help.validator(password, returnStr, 'password'))
+
+  if (!passwordConfirm)
+    return res.json(help.validator(passwordConfirm, returnStr, 'passwordConfirm'))
+
+  if (!phone)
+    return res.json(help.validator(phone, returnStr, 'phone'))
+
+  if (!code)
+    return res.json(help.validator(code, returnStr, 'code'))
+
+  if (!phoneCode)
+    return res.json(help.validator(phoneCode, returnStr, 'phoneCode'))
+
+  if (!username)
+    return res.json(help.validator(username, returnStr, 'username'))
+
+
+  // console.log(req.body.params)
 
   // 第1种方式：实例化了一个学生类，并，保存这个学生类
-  await new User(req.body.params).save(function () {
-    console.log("存储成功")
-    res.status(200).json({
-      status: '成功！OK'
+  try {
+    new User(req.body.params).save(function () {
+      console.log("存储成功")
+      res.status(200).json({
+        status: '成功！OK'
+      })
     })
-  })
+  } catch (err) {
+    return res.json(err)
+  }
+
   // 第2种方式：工厂方式，静态方法。
-  // User.create(req.query, function (error) {
-  //   console.log("保存成功！共6条！")
-  //   res.status(200).json({
-  //     status: '成功！OK'
+  // try {
+  //   User.create(req.query, function (error) {
+  //     console.log("保存成功！共6条！")
+  //     res.status(200).json({
+  //       status: '成功！OK'
+  //     })
   //   })
-  // })
+  // } catch (err) {
+  //   return res.json(err)
+  // }
+
 
 })
 
