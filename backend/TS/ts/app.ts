712e9b15c28b95fd6c357e73d5c53b398d2c00b5
mgb <mgbmkw@163.com>
2018-02-04 01:15:29 +0800

    完成初步测试

--- backend/TS/ts/app.ts
+++ backend/TS/ts/app.ts
@@ -1,9 +1,12 @@
-const abc = '666'
-console.log(abc)
-console.log(abc)
-console.log(abc)
-console.log(abc)
+import * as path from 'path'
+import * as express from 'express'
+import * as bodyParser from 'body-parser'
+import * as cookieParser from 'cookie-parser'
+import * as session from 'express-session'
+import * as favicon from 'serve-favicon'
+import * as swig from 'swig'
 
+const str = ''
 async function fn() {
-  let abc = await '999'
+  let str: string = await '10086'
 }
