0195a777ff4eb4c290b284ec3f9018dd2fd076a2
mgb <mgbmkw@163.com>
2018-02-06 02:55:52 +0800

    100

--- backend/TS/ts/app.ts
+++ backend/TS/ts/app.ts
@@ -6,7 +6,16 @@ import * as session from 'express-session'
 import * as favicon from 'serve-favicon'
 import * as swig from 'swig'
 
-const str = ''
-async function fn() {
-  let str: string = await '10086'
+
+
+let abc: abc100.myLib = {
+  name: '',
+  length: 666
 }
+
+function fn(str: string, num ? : number): string {
+  return str
+}
+console.log(abc)
+fn('789')
+console.log(fn('kkk'))
